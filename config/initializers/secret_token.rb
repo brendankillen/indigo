8f7eecb8cd3ec832257fb24fb804eced05bd5a62
Brendan Killen <brendankillen@gmail.com>
2013-05-19 12:13:42 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indigo::Application.config.secret_token = '542c16691f0447a2bc8fa96c4f895fb186911844b9052fb717433158a7467c6bf4fc9c091f191d044abac8eb63e8cc3a93e98e5df8b5d1b6deb70f6998a87291'
